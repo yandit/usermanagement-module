fb735f16d13a0cf22fc158ddf2e9b9570a085b4f
yudit <wayanyuditya@gmail.com>
2022-07-19 19:03:33 +0800

    laravel 7

--- Entities/Role.php
+++ Entities/Role.php
@@ -3,11 +3,9 @@
 namespace Modules\UserManagement\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
-    use HasFactory;
 
     protected $fillable = [
         'name',
--- Entities/RoleUser.php
+++ Entities/RoleUser.php
@@ -3,11 +3,9 @@
 namespace Modules\UserManagement\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RoleUser extends Model
 {
-    use HasFactory;
     protected $table = 'role_users';
     protected $fillable = [
         'role_id',
