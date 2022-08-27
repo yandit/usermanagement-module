f84306d26f4b408b948b03099dd8a3529e72ce28
Yudit <wayanyuditya@gmail.com>
2022-08-27 21:59:52 +0800

    fix helper

--- Helper/helper.php
+++ Helper/helper.php
@@ -1,36 +1,39 @@
 <?php
 
-function setMenuSession($role){
-    $menus = config('menus');
-    $menus = collect($menus);
-    $menus = $menus->sortBy('order');
+if(!function_exists('setMenuSession')){
+    function setMenuSession($role){
+        $menus = config('menus');
+        $menus = collect($menus);
+        $menus = $menus->sortBy('order');
+        
+        $permissions = ($role) ? $role->permissions : [];
+        $myMenus = [];
+        foreach($menus as $menu){
+            if(isset($menu['subs']) && $menu['subs']){
+                $ngsubs = [];
+                foreach($menu['subs'] as $k=>$v){
+                    if(isset($permissions[$v['permission']]) && $permissions[$v['permission']]){
+                        $ngsubs[] = $v;
+                    }
+                }
     
-    $permissions = ($role) ? $role->permissions : [];
-    $myMenus = [];
-    foreach($menus as $menu){
-        if(isset($menu['subs']) && $menu['subs']){
-            $ngsubs = [];
-            foreach($menu['subs'] as $k=>$v){
-                if(isset($permissions[$v['permission']]) && $permissions[$v['permission']]){
-                    $ngsubs[] = $v;
+                if($ngsubs){
+                    $menu['subs'] = $ngsubs;
+                    $myMenus[] = $menu;
+                }
+            }else{
+                if(isset($permissions[$menu['permission']]) && $permissions[$menu['permission']]){
+                    $myMenus[] = $menu;
                 }
             }
-
-            if($ngsubs){
-                $menu['subs'] = $ngsubs;
-                $myMenus[] = $menu;
-            }
-        }else{
-            if(isset($permissions[$menu['permission']]) && $permissions[$menu['permission']]){
-                $myMenus[] = $menu;
-            }
         }
+    
+        session(['menus' => $myMenus]);  
     }
-
-    session(['menus' => $myMenus]);  
 }
 
-function loggedInUser($key)
-{
-    return \Sentinel::getUser()[$key];
+if(!function_exists('loggedInUser')){
+    function loggedInUser($key){
+        return \Sentinel::getUser()[$key];
+    }
 }
